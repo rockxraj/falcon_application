965bec241c3db35c9128e5c42154503a3fad1388
Rajendra <ttt@tttui-Mac-mini.local>
2017-11-01 11:20:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import falcon
+
+class TestResource(object):
+    def on_get(self, req, res):
+        """Handles all GET requests."""
+        res.status = falcon.HTTP_200  # This is the default status
+        res.body = ('This is me, Falcon, serving a resource!')
+
+# Create the Falcon application object
+app = falcon.API()
+
+# Instantiate the TestResource class
+test_resource = TestResource()
+
+# Add a route to serve the resource
+app.add_route('/test', test_resource)
