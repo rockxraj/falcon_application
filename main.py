a51384842b3e28d49af47a6ba484f256fdaef399
Rajendra <ttt@tttui-Mac-mini.local>
2017-11-01 17:36:22 +0530

    database added

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import falcon
 import json
+
+from peewee import *
+
+db = SqliteDatabase('chatBot.db')
+
+class Chat(Model):
+    msg = CharField(max_length=255)
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+if __name__ == '__main__':
+    db.connect()
+    db.create_tables([Chat])
+
 class TestResource(object):
     def on_get(self, req, res):
         """Handles all GET requests."""
         res.status = falcon.HTTP_200  # This is the default status
-        res.body = ('This is me, Falcon, serving a resource and Deployed!')
-
+        result =  Chat.select().get()
+        res.body = result
+        
     def on_post(self, req, res):
         res.status = falcon.HTTP_200
         data = req.stream.read()
+        msgdata = Chat(msg=data)
+        msgdata.save()
         res.body = data
 
 
