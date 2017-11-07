4f9c8f83122115b136a3d3e3782434c3a5fdf5ce
Rajendra <ttt@tttui-Mac-mini.local>
2017-11-07 09:50:50 +0530

    added post request

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ class TestResource(object):
         res.status = falcon.HTTP_200
         data = req.stream.read() 
         #sendMessage(data[6], data[8], "Hello Raj!!")
-        print(json.dumps(data.decode("utf-8")))
+        a = json.dumps(data.decode("utf-8"))
+        print(json.loads(a))
         print(data)
         res.body = "posted"
         
